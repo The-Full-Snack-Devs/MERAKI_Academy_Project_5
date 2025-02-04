69a957270d4aa61f45cea44c0eafaf57fc451179
NatherBaneSalameh <natherbanisalameh@gmail.com>
2025-02-04 14:17:18 -0800

    get all services function

--- frontend/src/components/redux/store.js
+++ frontend/src/components/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/auth";
+import servicesReduser from "./reducers/services/index"
 
 
 const store = configureStore({
   reducer: {
-    authReducer: authReducer
+    authReducer: authReducer,
+    servicesReduser:servicesReduser
   },
 });
 
