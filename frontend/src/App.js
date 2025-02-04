af6051a0591b80e638a11cc535fa5c5d9f9c907d
NatherBaneSalameh <natherbanisalameh@gmail.com>
2025-02-04 13:13:47 -0800

    static home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-
+import Home from "./components/Home"
+import Services from "./components/Services";
 //===============================================================
 
 const App = () => {
   return (
     <div className="App">
+      <Services/>
       <Routes>
-        <Route path={"/"} element={<Register />} />
+     
       </Routes>
     </div>
   );
