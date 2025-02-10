e0f0cf75c335dd3c15d0dcea0d22c19e17be540e
NatherBaneSalameh <natherbanisalameh@gmail.com>
2025-02-10 10:16:21 -0800

    Dont Merg this

--- frontend main/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import './App.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from "./Routers"
-function App() {
-  return <RouterProvider router={router}/>
-}
-
-export default App
